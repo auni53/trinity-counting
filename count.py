ce2729c20e1de463133c1c233d8276b1112e6439
Auni Ahsan <auni.ahsan@shopify.com>
2017-03-17 11:47:17 -0400

    2017 fixes

--- count.py
+++ count.py
@@ -3,38 +3,38 @@ import csv
 import sys
 from os import listdir
 from os.path import isfile, join
+import pdb
+
+stop = pdb.set_trace
 
 def process(filename):
   '''
   filename -> (title, candidates, ballots)
   '''
 
-  try:
-    with open(filename) as file:
-      title = 0
-      start = 0
-      ballots = []
+  with open(filename) as file:
+    title = 0
+    start = 0
+    ballots = []
+  
+    csv_file = csv.reader(file, delimiter='\t')
+    lines = list(csv_file)
 
-      csv_file = csv.reader(file, delimiter='\t')
-      lines = list(csv_file)
-      for l in lines:
-        newL = []
-        if start == 1:
-          ballots.append(l[2:])
-        if title == 0:
-          title = l[0]
-        if l != [] and l[0] == 'socialyos':
-          start = 1
-        if (len(ballots) == 40 and False):
-          break
-      candidates = []
-      for b in ballots:
-        if (not b[0] in candidates):
-          candidates.append(b[0])
-      return (title, candidates, ballots)
-  except FileNotFoundError:
-    return False
+    for l in lines:
+      if start == 1:
+        ballots.append(l)
+      if title == 0:
+        title = l[0]
+        winner_i = l.index('Num1')
+      if l != [] and l[0] == 'socialyos':
+        start = 1
 
+    candidates = []
+    for b in ballots:
+      if (not b[winner_i] in candidates):
+        candidates.append(b[winner_i])
+    return (title, candidates, ballots)
+  
 def getBlankTally(candidates):
   tally = {}
   for name in candidates:
@@ -63,8 +63,8 @@ def isDone(tally):
   return False
 
 def count(title, candidates, ballots, n=1):
-  # print()
-  # print("~~%s~~" % title)
+  print()
+  print("~~%s~~" % title)
   winners = []
   tables = []
   while n > 0:
@@ -81,6 +81,8 @@ def count(title, candidates, ballots, n=1):
     r = 1
 
     while not isDone(tally): # len(validCandidates) > 1:
+
+      print("\nROUND %d" % r)
       tally = getBlankTally(validCandidates)
       validBallots = filter(validBallots, validCandidates)
       for b in validBallots:
@@ -90,26 +92,19 @@ def count(title, candidates, ballots, n=1):
           print('--KEY ERROR--')
           print(title)
           print(validCandidates)
-    #   print()
 
-      try:
-          least = min(tally.values())
-          for key in tally:
-            if tally[key] == least:
-              validCandidates.remove(key)
-      except ValueError:
-          print('--VALUE ERROR--')
-          print(title)
-          print(validCandidates)
-          return
+      least = min(tally.values())
+      for key in tally:
+        if tally[key] == least:
+          validCandidates.remove(key)
 
       for key in candidates:
         if key in tally:
           table[key].append(tally[key])
-        #   print(key, tally[key])
+          print(key, tally[key])
         else:
           table[key].append('*')
-        #   print(key, '*')
+          print(key, '*')
       r += 1
     tables.append(table)
     winners.append(isDone(tally))
@@ -133,40 +128,26 @@ if __name__ == '__main__':
 
   results = {}
 
-  if len(sys.argv) >= 2:
-    filename = str(sys.argv[1])
-    if len(sys.argv) == 3:
-      positions = int(sys.argv[2])
-    else:
-      positions = 1
-    data = process(filename)
-    if data:
-      print(data[0], data[1], positions)
-      count(data[0], data[1], data[2], positions)
-    else:
-      print("File not found.")
-  else:
-    mypath = 'week2'
-    fileList = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0] != '.' and f.split('.')[1] == 'xls')]
+  mypath = 'results/'
+  fileList = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0] != '.' and f.split('.')[1] == 'xls')]
+  
+  for filename in fileList:
+    try:
+      data = process(mypath + filename)
+    except UnicodeDecodeError:
+      print("--ERROR--")
+      print(filename)
 
-    # fileList = ['../excelfile (21).xls']
-    for filename in fileList:
-      try:
-          data = process('week2/' + filename)
-      except UnicodeDecodeError:
-          print("--ERROR--")
-          print(filename)
+    # temp bc no position name look up
+    data = (filename.split('.')[0], data[1], data[2])
+    result = count(data[0], data[1], data[2])
 
-    #   print(data[0], filename)
-      if (getN(data[0]) == 3 or True):
-          result = count(data[0], data[1], data[2], getN(data[0]))
-
-          c = 0
-          while c < len(result):
-            title = data[0]
-            if len(result) > 1:
-              title += ' Winner #' + str(c + 1)
-            results[title] = result[c]
-            c += 1
-
-    print(json.dumps(results))
+    c = 0
+    while c < len(result):
+      title = data[0]
+      if len(result) > 1:
+        title += ' Winner #' + str(c + 1)
+      results[title] = result[c]
+      c += 1
+  
+  # print(json.dumps(results))
